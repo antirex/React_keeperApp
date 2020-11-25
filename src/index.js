96420b078b47c6c8ff2779fdd2b4fc4538c9aaa2
antirex <anshulbamb22@gmail.com>
2020-11-25 13:55:01 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1 className = "heading">Hello World, I'm here and you too!</h1>,document.getElementById("root")
+);
+
+
