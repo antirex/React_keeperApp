0ab1060b4b969517fc3f59f9bf0c503de6ac4fb4
Anshul Bamb <anshulbamb22@gmail.com>
2020-11-28 13:19:13 +0530

    mapping data w/o fat arr

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notesFull";
 
-function App(){
-    return <div>
-        <Header />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        
-        <Footer />
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className = "property">
+        {notes.map(function (note) {
+            return <Note key={note.key} head={note.title} desc={note.content} />;
+        })}
+      </div>
+      <Footer />
     </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Note(){
-    return <div className = "notes">
-        <div className = "notes-body">
-            <h3>Note-heading</h3>
-            <p>anshul anshi is good jdaj janshul is aj lorem ispum dorem text random</p>
-            <hr className = "hr-line"/>
-        </div>
+function Note(props) {
+  return (
+    <div className="notes">
+      <div className="notes-body">
+        <h3>{props.head}</h3>
+        <p>{props.desc}</p>
+        <hr className="hr-line" />
+      </div>
     </div>
+  );
 }
 
 export default Note;
